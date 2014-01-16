2554cafbc66462ab3b5aa8cefc141554cc2d0d8e
Gary A. Stafford <garystafford@rochester.rr.com>
2014-01-16 00:19:23 -0500

    adding enable tunneling to recipe and adding recipe run list to default recipe

--- cookbooks/dev-setup/recipes/default.rb
+++ cookbooks/dev-setup/recipes/default.rb
@@ -7,9 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe	create-swapfile
 include_recipe	install-java
 include_recipe	install-env-vars
+include_recipe	create-swapfile
 include_recipe	install-wls
-include_recipe	create-wls-domain
 #include_recipe	remove-swapfile
+include_recipe	create-wls-domain
